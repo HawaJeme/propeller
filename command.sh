ef885254d7cfcd2d0effb99cd8a88fdc25f55db1
Hawa Jeme <kuuzey46@gmail.com>
2023-06-29 12:39:15 +0300

    add command.sh file

---
+++ command.sh
@@ -0,0 +1,10 @@
+1. create react app
+2. create github repo by: git init, git remote add origin <REMOTE_URL>, git push origin main
+3. create update_logo branch
+4. change logo.svg
+5. replace link with https://www.propelleraero.com/dirtmate/
+6. push the code
+7. create a pull request to main branch
+8. merge the pull request
+
+REPO_URL : https://github.com/kuzey4/propeller
